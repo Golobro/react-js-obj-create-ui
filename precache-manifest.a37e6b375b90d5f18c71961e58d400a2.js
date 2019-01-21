75cc340a61538299fab39d979c8ff1aad83200fc
Golobro <glogvirobo@gmail.com>
2019-01-22 01:03:46 +1100

    Updates

---
+++ precache-manifest.a37e6b375b90d5f18c71961e58d400a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "235684a42756def3fd45",
+    "url": "/react-js-obj-create-ui/static/js/runtime~main.235684a4.js"
+  },
+  {
+    "revision": "c2ee619f1be22d51c2a1",
+    "url": "/react-js-obj-create-ui/static/js/main.c2ee619f.chunk.js"
+  },
+  {
+    "revision": "0148014f8f40fab6133c",
+    "url": "/react-js-obj-create-ui/static/js/1.0148014f.chunk.js"
+  },
+  {
+    "revision": "c2ee619f1be22d51c2a1",
+    "url": "/react-js-obj-create-ui/static/css/main.665a3dc8.chunk.css"
+  },
+  {
+    "revision": "3521321d4fbbe31a500ebda639ac9c88",
+    "url": "/react-js-obj-create-ui/index.html"
+  }
+];
